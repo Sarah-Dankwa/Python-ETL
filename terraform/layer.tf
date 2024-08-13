ba24c867976ff927776c624b76f095bb9111d083
MIN <meengrace@gmail.com>
2024-08-14 00:17:54 +0100

    change makefile and run it successful, amend s3.tf, create layer.tf to deploy layers for pandas package dependencies in aws successful, open to add extra other additional packages depending on future needs

---
+++ terraform/layer.tf
@@ -0,0 +1,26 @@
+resource "null_resource" "create_dependencies_pandas" {
+  provisioner "local-exec" {
+    command = "make -C ${path.module}/.. custom-dependencies"
+  }
+
+  triggers = {
+    requirements_hash = filemd5("${path.module}/../requirements.txt")
+    makefile_hash     = filemd5("${path.module}/../Makefile")
+  }
+}
+
+data "archive_file" "layer_code" {
+  type        = "zip"
+  output_path = "${path.module}/../packages/layer/layer.zip"
+  source_dir  = "${path.module}/../dependencies"
+
+#   depends_on = [null_resource.create_dependencies] 
+}
+
+#need iam permission of lambda:CreateLayerVersion, or maybe lambda:DeleteLayerVersion
+resource "aws_lambda_layer_version" "dependencies" {
+  layer_name = "pandas_library_layer"
+  s3_bucket  = aws_s3_object.lambda_layer.bucket
+  s3_key     = aws_s3_object.lambda_layer.key
+}
+
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,3 +5,19 @@ resource "aws_s3_bucket" "ingested_data_bucket" {
 resource "aws_s3_bucket" "processed_data_bucket" {
   bucket_prefix = "nc-de-totesys-processed-data-"
 }
+
+resource "aws_s3_bucket" "code_bucket" {
+  bucket_prefix = "nc-de-totesys-code-"
+}
+
+#need permission of uploading objects; s3:PutObject
+resource "aws_s3_object" "lambda_layer" {
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key    = "layer/layer.zip"
+  source = data.archive_file.layer_code.output_path
+  
+  etag   = filemd5(data.archive_file.layer_code.output_path)
+
+  depends_on = [ data.archive_file.layer_code ]
+  
+}
