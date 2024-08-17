5766cf4f1dcb0f8e39c21047ea93d0d1c3e6b497
Sarah-Dankwa <sarah_dankwa@hotmail.co.uk>
2024-08-17 12:56:31 +0100

    Added lifecycle rule to sns resources

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -35,6 +35,9 @@ resource "aws_cloudwatch_metric_alarm" "extract_metric_alarm" {
 // Create a SNS topic
 resource "aws_sns_topic" "extract_metric_alarm_updates" {
   name = "user-updates-topic"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 
@@ -44,4 +47,19 @@ resource "aws_sns_topic_subscription" "extract_metric_alarm_email_target" {
   topic_arn = aws_sns_topic.extract_metric_alarm_updates.arn
   protocol  = "email"
   endpoint  = each.value
+  lifecycle {
+    prevent_destroy = true
+  }
 }
+
+
+# created sns topic and subscription in console and imported into terraform infrastructure file
+# import {
+#   to = aws_sns_topic.user_updates
+#   id = "arn:aws:sns:us-west-2:0123456789012:my-topic"
+# }
+
+# import {
+#   to = aws_sns_topic_subscription.user_updates_email_target
+#   id = "arn:aws:sns:us-west-2:0123456789012:my-topic:8a21d249-4329-4871-acc6-7be709c6ea7f"
+# }
