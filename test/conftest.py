c729cc7f7a48a10ab192d0a292dd707d2d93c3ae
Pringading <jess.marcell@gmail.com>
2024-08-21 12:03:19 +0100

    complete load lambda handler and tests

--- test/conftest.py
+++ test/conftest.py
@@ -4,8 +4,10 @@ import boto3
 from moto import mock_aws
 from dotenv import load_dotenv
 import json
+from db.connection import connect_to_db
 from unittest.mock import patch
 from datetime import datetime
+from db.seed import seed_warehouse
 
 
 @pytest.fixture(scope="session")
@@ -38,7 +40,9 @@ def now_variable():
 
 
 @pytest.fixture
-def secretsmanager_client(aws_credentials, environment_variables):
+def secretsmanager_client(
+    aws_credentials, environment_variables, secretsmanager_client_test
+):
     """mock boto3 secretsmanager client with db credentials from local .env"""
     # load_dotenv()
     secret = {}
@@ -88,6 +92,7 @@ def s3_client(aws_credentials):
 
 @pytest.fixture
 def table_names():
+    """table names in the totesys OLTP database"""
     tables = [
         "address",
         "staff",
@@ -102,3 +107,59 @@ def table_names():
         "design",
     ]
     return tables
+
+
+@pytest.fixture
+def valid_warehouse_credentials(aws_credentials, environment_variables):
+    """mock boto3 secretsmanager client with warehouse credentials from local
+    .env"""
+
+    secret = {}
+    secret["Database"] = os.environ["LOCAL_DATABASE"]
+    secret["Hostname"] = os.environ["LOCAL_HOST"]
+    secret["Username"] = os.environ["LOCAL_USER"]
+    secret["Password"] = os.environ["LOCAL_PASSWORD"]
+    secret["Port"] = os.environ["LOCAL_PORT"]
+    json_secret = json.dumps(secret)
+    with mock_aws():
+        client = boto3.client("secretsmanager", region_name="eu-west-2")
+        client.create_secret(Name="totesys-warehouse", SecretString=json_secret)
+        yield client
+
+
+@pytest.fixture
+def invalid_warehouse_credentials(secretsmanager_client_test):
+    """invalid database connections added to mock secrets manager to test
+    error handling
+    """
+
+    secret = {}
+    secret["Database"] = "invalid_db"
+    secret["Hostname"] = "invalid_host"
+    secret["Username"] = "invalid_user"
+    secret["Password"] = "invalid_password"
+    secret["Port"] = "invalid_port"
+    json_secret = json.dumps(secret)
+    secretsmanager_client_test.create_secret(
+        Name="totesys-warehouse", SecretString=json_secret
+    )
+    yield secretsmanager_client_test
+
+
+@pytest.fixture
+def conn():
+    """connects to local database & adds empty tables
+    
+    yields a connection to the local database 
+    then closes the connection after each test is complete
+    """
+
+    db = None
+    try:
+        db = connect_to_db()
+        seed_warehouse(db)
+        yield db
+    finally:
+        if db:
+            db.close()
+
