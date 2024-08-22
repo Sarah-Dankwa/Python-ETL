f3199c5c2974f57d96e47dfc7f726438a42b1855
Pringading <jess.marcell@gmail.com>
2024-08-22 16:01:38 +0100

    add data for test oltp warehouse

---
+++ db/oltp_data/address.py
@@ -0,0 +1,122 @@
+address = [
+    {
+        "address_id": 1,
+        "address_line_1": "6826 Herzog Via",
+        "address_line_2": None,
+        "district": "Avon",
+        "city": "New Patienceburgh",
+        "postal_code": "28441",
+        "country": "Turkey",
+        "phone": "1803 637401",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 2,
+        "address_line_1": "179 Alexie Cliffs",
+        "address_line_2": None,
+        "district": None,
+        "city": "Aliso Viejo",
+        "postal_code": "99305-7380",
+        "country": "San Marino",
+        "phone": "9621 880720",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 3,
+        "address_line_1": "148 Sincere Fort",
+        "address_line_2": None,
+        "district": None,
+        "city": "Lake Charles",
+        "postal_code": "89360",
+        "country": "Samoa",
+        "phone": "0730 783349",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 4,
+        "address_line_1": "6102 Rogahn Skyway",
+        "address_line_2": None,
+        "district": "Bedfordshire",
+        "city": "Olsonside",
+        "postal_code": "47518",
+        "country": "Republic of Korea",
+        "phone": "1239 706295",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 5,
+        "address_line_1": "34177 Upton Track",
+        "address_line_2": None,
+        "district": None,
+        "city": "Fort Shadburgh",
+        "postal_code": "55993-8850",
+        "country": "Bosnia and Herzegovina",
+        "phone": "0081 009772",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 6,
+        "address_line_1": "846 Kailey Island",
+        "address_line_2": None,
+        "district": None,
+        "city": "Kendraburgh",
+        "postal_code": "08841",
+        "country": "Zimbabwe",
+        "phone": "0447 798320",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 7,
+        "address_line_1": "75653 Ernestine Ways",
+        "address_line_2": None,
+        "district": "Buckinghamshire",
+        "city": "North Deshaun",
+        "postal_code": "02813",
+        "country": "Faroe Islands",
+        "phone": "1373 796260",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 8,
+        "address_line_1": "0579 Durgan Common",
+        "address_line_2": None,
+        "district": None,
+        "city": "Suffolk",
+        "postal_code": "56693-0660",
+        "country": "United Kingdom",
+        "phone": "8935 157571",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 9,
+        "address_line_1": "644 Edward Garden",
+        "address_line_2": None,
+        "district": "Borders",
+        "city": "New Tyra",
+        "postal_code": "30825-5672",
+        "country": "Australia",
+        "phone": "0768 748652",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    },
+    {
+        "address_id": 10,
+        "address_line_1": "49967 Kaylah Flat",
+        "address_line_2": "Tremaine Circles",
+        "district": "Bedfordshire",
+        "city": "Beaulahcester",
+        "postal_code": "89470",
+        "country": "Democratic People's Republic of Korea",
+        "phone": "4949 998070",
+        "created_at": "2022-11-03 14:20:49.962000",
+        "last_updated": "2022-11-03 14:20:49.962000"
+    }
+]
---
+++ db/oltp_data/counterparty.py
@@ -0,0 +1,92 @@
+counterparty = [
+    {
+        "counterparty_id": 1,
+        "counterparty_legal_name": "Fahey and Sons",
+        "legal_address_id": 5,
+        "commercial_contact": "Micheal Toy",
+        "delivery_contact": "Mrs. Lucy Runolfsdottir",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 2,
+        "counterparty_legal_name": "Leannon, Predovic and Morar",
+        "legal_address_id": 8,
+        "commercial_contact": "Melba Sanford",
+        "delivery_contact": "Jean Hane III",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 3,
+        "counterparty_legal_name": "Armstrong Inc",
+        "legal_address_id": 2,
+        "commercial_contact": "Jane Wiza",
+        "delivery_contact": "Myra Kovacek",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 4,
+        "counterparty_legal_name": "Kohler Inc",
+        "legal_address_id": 9,
+        "commercial_contact": "Taylor Haag",
+        "delivery_contact": "Alfredo Cassin II",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 5,
+        "counterparty_legal_name": "Frami, Yundt and Macejkovic",
+        "legal_address_id": 2,
+        "commercial_contact": "Homer Mitchell",
+        "delivery_contact": "Ivan Balistreri",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 6,
+        "counterparty_legal_name": "Mraz LLC",
+        "legal_address_id": 3,
+        "commercial_contact": "Florence Casper",
+        "delivery_contact": "Eva Upton",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 7,
+        "counterparty_legal_name": "Padberg, Lueilwitz and Johnson",
+        "legal_address_id": 6,
+        "commercial_contact": "Ms. Wilma Witting",
+        "delivery_contact": "Christy O'Hara",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 8,
+        "counterparty_legal_name": "Grant - Lakin",
+        "legal_address_id": 6,
+        "commercial_contact": "Emily Orn",
+        "delivery_contact": "Veronica Fay",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 9,
+        "counterparty_legal_name": "Price LLC",
+        "legal_address_id": 5,
+        "commercial_contact": "Sheryl Langworth",
+        "delivery_contact": "Simon Schoen",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    },
+    {
+        "counterparty_id": 10,
+        "counterparty_legal_name": "Bosco - Grant",
+        "legal_address_id": 10,
+        "commercial_contact": "Ed Halvorson",
+        "delivery_contact": "Dewey Kuhic",
+        "created_at": "2022-11-03 14:20:51.563000",
+        "last_updated": "2022-11-03 14:20:51.563000"
+    }
+]
